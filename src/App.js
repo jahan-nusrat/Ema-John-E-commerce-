9d9d5590945f94a8c3510e39ec2e88a628e4d1ff
Nusrat Jahan <njnusrat53@gmail.com>
2020-10-21 22:26:51 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import Home from './components/Home/Home/Home';
+
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
+
+export default App
